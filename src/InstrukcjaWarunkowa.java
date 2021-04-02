30cc185e17107b95e648c6dad5518aa3824757a7
MateuszRusinowicz <mateuszrusinowicz@gmail.com>
2021-04-02 11:32:43 +0200

    InstrukcjaWarunkuwa

---
+++ src/InstrukcjaWarunkowa.java
@@ -0,0 +1,11 @@
+//Instrukcje warunkowe pozwalają nam określić, które części kodu mają zostać wukonane gdy zostane spełniony warunek
+public class InstrukcjaWarunkowa {
+    public static void main(String[] args) {
+        float dzielna = 4.0F;
+        float dzielnik = 2.0F;
+        if (dzielnik != 0) {// JEŻELI DZIELNIK JEST RÓŻNY OD "0"
+            System.out.println("Wynik dzielenia to: " + (dzielna / dzielnik));
+        }
+        System.out.println("Proszę podaj inną liczbę");//NIE POWINNO SIĘ WYŚIETLAĆ- PATRZ NASTEPNA LEKCJA
+    }
+}
