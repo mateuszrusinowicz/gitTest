4aaa8ee769212153adea20e2641e447f65b18a22
MateuszRusinowicz <mateuszrusinowicz@gmail.com>
2021-04-02 12:42:30 +0200

    Instrukcja warunkowa if oraz else

--- src/InstrukcjaWarunkowa.java
+++ src/InstrukcjaWarunkowa.java
@@ -5,7 +5,9 @@ public class InstrukcjaWarunkowa {
         float dzielnik = 2.0F;
         if (dzielnik != 0) {// JEŻELI DZIELNIK JEST RÓŻNY OD "0"
             System.out.println("Wynik dzielenia to: " + (dzielna / dzielnik));
+        } else{//zostanie uruchomiona tyko wtedy kiedy dziielnik będzie równy 0
+            System.out.println("Proszę podaj inną liczbę");//NIE POWINNO SIĘ WYŚIETLAĆ- PATRZ NASTEPNA LEKCJA
+
         }
-        System.out.println("Proszę podaj inną liczbę");//NIE POWINNO SIĘ WYŚIETLAĆ- PATRZ NASTEPNA LEKCJA
     }
 }
